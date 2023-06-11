300c9730aaf14685c770857cfc6834d900939bf1
ornytula <120215872+ornytula@users.noreply.github.com>
2023-06-11 20:48:53 +0300

    Add files via upload

---
+++ js/demo1.js
@@ -0,0 +1,17 @@
+let nav = document.querySelector('.nav')
+
+let tween = gsap.to(".purple", {
+  duration: 4, 
+  x: () => nav.offsetWidth, // animate by the px width of the nav
+  xPercent: -100, // offset by the width of the box
+  rotation: 360, 
+  ease: "none", 
+  paused: true
+});
+
+// click handlers for controlling the tween instance...
+document.querySelector("#play").onclick = () => tween.play();
+document.querySelector("#pause").onclick = () => tween.pause();
+document.querySelector("#resume").onclick = () => tween.resume();
+document.querySelector("#reverse").onclick = () => tween.reverse();
+document.querySelector("#restart").onclick = () => tween.restart();
